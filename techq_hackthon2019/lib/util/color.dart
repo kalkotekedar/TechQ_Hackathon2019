2480dd2e8bc77e330e12ad688ec8bd7a0dd55591
Kedar Kalkote <kedark@incubxperts.com>
2019-06-01 10:49:04 +0530

    Initial commit

---
+++ techq_hackthon2019/lib/util/color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const Color DarkPrimaryColor = Color(0xFF689F38);
+const Color LightPrimaryColor = Color(0xFFDCEDC8);
+const Color PrimaryColor = Color(0xFF8BC34A);
+const Color TextIcon = Color(0xFF212121);
+const Color AccentColor = Color(0xFFFFC107);
+const Color PrimaryText = Color(0xFF212121);
+const Color SecondaryText = Color(0xFF757575);
+const Color DividerColor = Color(0xFFBDBDBD);
