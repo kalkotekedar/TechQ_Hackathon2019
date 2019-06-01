e2a881ba34026a3443a496a53a535df83c21d6ca
AkashDanao <akash.danao@globant.com>
2019-06-01 14:16:17 +0530

    create list Please enter the commit message for your changes. Lines starting

--- techq_hackthon2019/lib/screens/first_page.dart
+++ techq_hackthon2019/lib/screens/first_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:techq_hackthon2019/util/constants.dart';
 import 'package:techq_hackthon2019/widgets/app_bar.dart';
+import 'dart:async';
+import 'package:http/http.dart' as http;
 
 class FirstPage extends StatefulWidget {
   @override
@@ -12,7 +14,12 @@ class _FirstPageState extends State<FirstPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(T_app_name),
-      body: Container(),
+      body: Container(
+      )
     );
   }
+
+
+      
+
 }
--- techq_hackthon2019/lib/screens/home_page.dart
+++ techq_hackthon2019/lib/screens/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:techq_hackthon2019/util/constants.dart';
-import 'package:techq_hackthon2019/util/style.dart';
 import 'package:techq_hackthon2019/widgets/app_bar.dart';
 
 class HomePage extends StatefulWidget {
@@ -36,7 +35,111 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
       ),
-      body: Container(),
+      body: Container(
+        child : ListView.builder(
+        itemCount: europeanCountries.length,
+        itemBuilder: (context, index) {
+          return buildListItem(europeanCountries[index]);
+        },
+      ),
+      ),
     );
   }
+
+  buildListItem(String title){
+    return Card(
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          children: <Widget>[
+              Container(
+                alignment: Alignment.center,
+                width: 50.0,
+                height: 50.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.grey
+                ),
+                child: Text('2',style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 15,
+                ),),),
+                Container(
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.only(right: 10, left: 10) ,
+                  child: Column(
+                    children: <Widget>[
+                      Text(title, style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20
+                      ),),
+                    Row(children: <Widget>[
+                      Padding(padding: EdgeInsets.all(10.0),
+            
+                      child : Container(
+                    alignment: Alignment.center,
+                    width: 80.0,
+                    height: 30.0,
+                    color: Colors.transparent,
+                    child: new Container(
+                      margin: EdgeInsets.only(left: 5.0, right: 5.0),
+                      decoration: new BoxDecoration(
+                      color: Colors.purpleAccent,
+                      borderRadius: new BorderRadius.all(
+                        const Radius.circular(10.0)),
+                          ),
+                    child: new Center(
+                      child: new Text("Python"),
+                    )),
+                )),
+                Container(
+                    alignment: Alignment.center,
+                    width: 80.0,
+                    height: 30.0,
+                    color: Colors.transparent,
+                    child: new Container(
+                        decoration: new BoxDecoration(
+                      color: Colors.purpleAccent,
+                      borderRadius: new BorderRadius.all(
+                        const Radius.circular(10.0)),
+                          ),
+                    child: new Center(
+                      child: new Text("Python"),
+                    )),
+                ),
+            
+                    ],)
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child : Container(
+                alignment: Alignment.center,
+                width: 50.0,
+                height: 50.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.deepOrange
+                ),
+                child: Text('2',style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 15,
+                ),),)),
+          ],
+        ),
+    )
+    );
+  }
+
+  final europeanCountries = ['Albania', 'Andorra', 'Armenia', 'Austria', 
+        'Azerbaijan', 'Belarus', 'Belgium', 'Bosnia and Herzegovina', 'Bulgaria',
+        'Croatia', 'Cyprus', 'Czech Republic', 'Denmark', 'Estonia', 'Finland',
+        'France', 'Georgia', 'Germany', 'Greece', 'Hungary', 'Iceland', 'Ireland',
+        'Italy', 'Kazakhstan', 'Kosovo', 'Latvia', 'Liechtenstein', 'Lithuania',
+        'Luxembourg', 'Macedonia', 'Malta', 'Moldova', 'Monaco', 'Montenegro',
+        'Netherlands', 'Norway', 'Poland', 'Portugal', 'Romania', 'Russia',
+        'San Marino', 'Serbia', 'Slovakia', 'Slovenia', 'Spain', 'Sweden', 
+        'Switzerland', 'Turkey', 'Ukraine', 'United Kingdom', 'Vatican City'];
+
 }
+
